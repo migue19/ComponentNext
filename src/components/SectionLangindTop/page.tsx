050c4d32be31014fdac9c18d0d820fe9b2167668
Aviel Aldama <aviel.aldama@gmail.com>
2023-02-26 20:24:39 -0600

    add top section

--- src/components/SectionLangindTop/page.tsx
+++ src/components/SectionLangindTop/page.tsx
@@ -1,7 +1,16 @@
+import styles from './styles.module.scss'
 
 const Page=() => {
     return (
-      <></>
+      <div className={styles.sectionLandingContainer}>
+        <div className={styles.sectionLandingContent}>
+            <h1>Lorem Ipsum</h1>
+            <p>It is a long established fact that a reader will be distracted by the 
+              readable content of a page when looking at its layout. 
+              The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+              as opposed to using Content here, content here, making it look like readable English.</p>
+        </div>
+      </div>
     )
 
 }
