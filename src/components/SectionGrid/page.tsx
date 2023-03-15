c8f9ce27489c87280bc549f4d6c05f8854ebf2c8
Aviel Aldama <aviel.aldama@gmail.com>
2023-03-15 14:31:18 -0600

    css grid

--- src/components/SectionGrid/page.tsx
+++ src/components/SectionGrid/page.tsx
@@ -7,27 +7,27 @@ const Page=() => {
 
           <div className={styles.containerGridContent}>
 
-            <div className=''>
+            <div className={styles.areaNavBar}>
               <p>Barra de Navegacion</p>
             </div>
             
-            <div className=''>
+            <div className={styles.areaLanding}>
               <p>Landing principal</p>
             </div>
             
-            <div className=''>
+            <div className={styles.sectionSecundario}>
               <p>landing secundario 1</p>
             </div>
 
-            <div className=''>
+            <div className={styles.sectionSecundario2}>
               <p>landing terciario 1</p>
             </div>
             
-            <div className=''>
+            <div className={styles.sectionVertical}>
               <p>section vertical</p>
             </div>
 
-            <div className=''>
+            <div className={styles.areaFooter}>
               <p>footer</p>
             </div>
 
